0357a315d10c95209eb6e29d5728d211e36f68b5
Bifard <Leks11172@mail.ru>
2024-07-11 17:32:52 +0700

    added view

--- src/App.tsx
+++ src/App.tsx
@@ -2,43 +2,25 @@ import "./App.css";
 
 import { useEffect, useState } from "react";
 
-import { io } from "socket.io-client";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-
 const socket2 = new WebSocket("ws://localhost:5100/Echo");
 //const socket = io("http://localhost:5100/Echo");
 function App() {
   const [isConnected, setIsConnected] = useState<boolean>();
   const [fooEvents, setFooEvents] = useState<any[]>([]);
   useEffect(() => {
-    /*    function onConnect() {
-      setIsConnected(true);
-    }
-    function onDisconnect() {
-      setIsConnected(false);
-    }
-    function onFooEvent(value: any) {
-      setFooEvents((previous) => [...previous, value]);
-    }
-    socket.on("connect", onConnect);
-    socket.on("disconnect", onDisconnect);
-    socket.on("foo", onFooEvent);
-    return () => {
-      socket.off("connect", onConnect);
-      socket.off("disconnect", onDisconnect);
-      socket.off("foo", onFooEvent);
-    }; */
     socket2.onopen = function () {
       console.log("Соединение установлено");
+      setIsConnected(true);
     };
 
     socket2.onmessage = function (event) {
       console.log(`Получено сообщение: ${event.data}`);
+      setFooEvents((prev) => [...prev, event]);
     };
 
-    socket2.onclose = function (event) {
+    socket2.onclose = function () {
       console.log("Соединение закрыто");
+      setIsConnected(false);
     };
 
     socket2.onerror = function (error) {
@@ -51,7 +33,16 @@ function App() {
     console.log("fooEvents : ", fooEvents);
   }, [isConnected, fooEvents]);
 
-  return <></>;
+  return (
+    <div>
+      <span>{`Подключение - ${isConnected}`}</span>
+      <ul>
+        {fooEvents.map((item, index) => {
+          return <li key={index}>{item.data}</li>;
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
